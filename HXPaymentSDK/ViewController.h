b901cf0c70d9993d52a6af49d2adfd527ff45c67
huangxiong <huangxionging@163.com>
2016-03-02 10:11:14 +0800

    Initial Commit

---
+++ HXPaymentSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXPaymentSDK
+//
+//  Created by huangxiong on 16/3/2.
+//  Copyright © 2016年 huangxiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
